e05c899c8398e3976fcf9f7063890a81d4d3af4f
JacobBanghart <jacobmbanghart@gmail.com>
2024-12-31 20:42:40 +0000

    fix: poorly copy pasted python

--- next_version.py
+++ next_version.py
@@ -6,15 +6,13 @@ next_tag = ""
 brokenup_tag = last_tag.split(".")
 
 if "major/" in last_commit:
-major_version = int(brokenup_tag[0])
-next_tag = str(major_version+1)+".0.0"
-
+    major_version = int(brokenup_tag[0])
+    next_tag = str(major_version+1)+".0.0"
 elif "feature/" in last_commit:
-feature_version = int(brokenup_tag[1])
-next_tag = brokenup_tag[0]+"."+str(feature_version+1)+".0"
-
+    feature_version = int(brokenup_tag[1])
+    next_tag = brokenup_tag[0]+"."+str(feature_version+1)+".0"
 else:
-patch_version = int(brokenup_tag[2])
-next_tag = brokenup_tag[0]+"."+brokenup_tag[1]+"."+str(patch_version+1)
+    patch_version = int(brokenup_tag[2])
+    next_tag = brokenup_tag[0]+"."+brokenup_tag[1]+"."+str(patch_version+1)
 
 print(next_tag)
